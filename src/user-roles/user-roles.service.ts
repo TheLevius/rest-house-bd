3c7707f643f2a1deac16bb8c5d00b937c19ccb92
TheLevius <reflevius@gmail.com>
2024-02-16 02:06:19 +0300

    fix: add exceptions handlers

--- src/user-roles/user-roles.service.ts
+++ src/user-roles/user-roles.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/db/prisma.service';
 import {
   UserRoleExtResponse,
@@ -32,47 +36,71 @@ export class UserRolesService {
     return result;
   }
   public async findAllByUserId(id: string): Promise<UserRoleResponse[]> {
-    const result = await this.prisma.userRole.findMany({
-      where: { userId: id },
-      select,
-    });
-    return result;
+    try {
+      const result = await this.prisma.userRole.findMany({
+        where: { userId: id },
+        select,
+      });
+      return result;
+    } catch (err) {
+      throw new NotFoundException(`User Role with id: ${id} not found`);
+    }
   }
   public async findAllByRole(title: string): Promise<UserRoleExtResponse[]> {
-    const result = await this.prisma.userRole.findMany({
-      where: {
-        role: {
-          title,
+    try {
+      const result = await this.prisma.userRole.findMany({
+        where: {
+          role: {
+            title,
+          },
         },
-      },
-      select,
-    });
-    return result;
+        select,
+      });
+      return result;
+    } catch (err) {
+      throw new NotFoundException(`User Role with title: ${title} not found`);
+    }
   }
   public async findRelationById(id: string): Promise<UserRoleResponse> {
-    const result = await this.prisma.userRole.findUniqueOrThrow({
-      where: { id },
-    });
-    return result;
+    try {
+      const result = await this.prisma.userRole.findUniqueOrThrow({
+        where: { id },
+      });
+      return result;
+    } catch (err) {
+      throw new NotFoundException(
+        `User-Role Relation with title: ${id} not found`,
+      );
+    }
   }
   public async create({
     userId,
     roleId,
   }: CreateUserRoleDto): Promise<UserRoleExtResponse> {
-    const result = await this.prisma.userRole.create({
-      data: {
-        userId,
-        roleId,
-      },
-      select,
-    });
-    return result;
+    try {
+      const result = await this.prisma.userRole.create({
+        data: {
+          userId,
+          roleId,
+        },
+        select,
+      });
+      return result;
+    } catch (err) {
+      throw new InternalServerErrorException(
+        'An unexpected error occurred on the server',
+      );
+    }
   }
 
   public async delete(id: string) {
-    const result = await this.prisma.userRole.delete({
-      where: { id },
-    });
-    return result;
+    try {
+      const result = await this.prisma.userRole.delete({
+        where: { id },
+      });
+      return result;
+    } catch (err) {
+      throw new NotFoundException(`User Role with id: ${id} not found`);
+    }
   }
 }

5524f3e77fbd5371a982eda20f42162711296d31
TheLevius <reflevius@gmail.com>
2024-03-16 15:33:54 +0300

    feat: add comments controller

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
 
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
+  @Get()
+  public getAll() {
+    return this.commentsService.findAll();
+  }
+  @Get(':id')
+  public getOneById(@Param('id', ParseUUIDPipe) id: string) {
+    return this.commentsService.findOneById(id);
+  }
+  @Post()
+  public create(@Body() dto: CreateCommentDto) {
+    return this.commentsService.create(dto);
+  }
+  @Put(':id')
+  public update(@Param('id', ParseUUIDPipe) id: string, dto: CreateCommentDto) {
+    return this.commentsService.update(id, dto);
+  }
+  @HttpCode(204)
+  @Delete(':id')
+  public delete(@Param('id', ParseUUIDPipe) id: string) {
+    return this.commentsService.delete(id);
+  }
 }
